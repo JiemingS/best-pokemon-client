b7d6ba5f3d4319d35d51f600ea4fb30ccf446f21
JiemingS <sunjieming1121@gmail.com>
2019-12-05 16:41:27 -0500

    change the url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://fathomless-shelf-81867.herokuapp.com',
+  production: 'https://whispering-brushlands-72580.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
